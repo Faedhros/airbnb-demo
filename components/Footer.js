f1ee1119a109aee5f0d0f8fe2c3ac2351020249b
ArmandoMaytorenas <armando.m@rivka.mx>
2021-08-04 17:27:22 -0700

    commit on main

---
+++ components/Footer.js
@@ -0,0 +1,49 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Demo sites</p>
+                <p>Referrals accepted</p>
+                <p>Next JS</p>
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>How airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help centre</p>
+                <p>Tolkien is my master</p>
+                <p>Trust & safety</p>
+                <p>Easter eggs</p>
+                <p>For honor</p>
+            </div>
+
+
+
+        </div>
+    )
+}
+export default Footer
